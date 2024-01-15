645197f71211a92123996328540b2b207330d0c1
cracrazjf <cracrazjf@gmail.com>
2024-01-15 13:22:56 -0600

    removed version control for setuptools

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     classifiers=[
         'Intended Audience :: Science/Research'],
     python_requires='>=3.6.8',
-    install_requires=['setuptools>=68.2.2',
+    install_requires=['setuptools',
                       'torch>=2.1.0.dev20230330',
                       'numpy>=1.24.1',
                       'pandas>=1.5.3',
